239203a384aeb8b93db6e67e90bd4f2d5e340cca
Yashsva <namansisodiya2013@gmail.com>
2020-07-29 15:14:49 +0530

    Made changes for deployment.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const https = require('https');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -7,6 +9,10 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
 
 
 const errorController = require('./controllers/error');
@@ -31,12 +37,15 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const privateKey=fs.readFileSync('server.key');
+const certificate=fs.readFileSync('server.cert');
+
 const imageFileStorage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    callback(null, new Date().getTime()+'-'+file.originalname);   //new Date().toISOString() + '-' +
+    callback(null, new Date().getTime() + '-' + file.originalname);   //new Date().toISOString() + '-' +
   }
 });
 //For multer-> fliter file types
@@ -61,13 +70,21 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(multer({ storage: imageFileStorage}).single('image'));
-app.use(multer({ storage: imageFileStorage,fileFilter:fileFilter }).single('image'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images',express.static(path.join(__dirname, 'images')));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
-app.use( 
+//set secure response headers
+app.use(helmet());
+//compress assets while sending response
+app.use(compression());
+//log the requests in a file
+app.use(morgan('combined',{ stream:accessLogStream}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: imageFileStorage, fileFilter: fileFilter }).single('image'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(
   session(
     {
       secret: 'my secret',
@@ -120,7 +137,7 @@ app.use((error, req, res, next) => {
   console.log('500 Error');
   // console.log(req);
   // console.log('Error', error);
-  
+
   res.status(500).render('500',
     {
       pageTitle: 'Error',
@@ -134,7 +151,11 @@ mongoose
   .then(result => {
 
     console.log('Connected to DB');
-    app.listen(3500);
+    // to use https
+    // https.createServer({key:privateKey,cert:certificate},app).listen(process.env.PORT || 3500);
+    
+    // to use http
+    app.listen(process.env.PORT || 3500);
   })
   .catch(err => {
     console.log(err);
