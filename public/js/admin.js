8af6c12d1a105a2c0333703c2ef8156f35c87533
Yashsva <namansisodiya2013@gmail.com>
2021-01-13 15:40:38 +0530

    bug fix - redirection after deletion

--- public/js/admin.js
+++ public/js/admin.js
@@ -19,7 +19,7 @@ const deleteProduct=(btn)=>{
     })
     .then((data)=>{
         console.log(data);
-        window.location.reload();
+        window.location.href="/admin/products";
         // productElement.parentNode.removeChild(productElement);
     })
     .catch(err=>console.log(err));
